62c91b6349c431499bb781f9c5c7c2e8d7b483a0
Landung 'Don' Setiawan <landungs@uw.edu>
2024-04-11 15:25:41 -0700

    feat: Add CLI for spinning up streamlit app
    Setup a simple gf-portal cli call to spin up the streamlit app
    --
    ref: #5

---
+++ src/genetic_forensic_portal/cli.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+import subprocess
+from pathlib import Path
+
+here = Path(__file__).parent
+home_file = here / "app/Home.py"
+
+def up():
+    subprocess.run(["python", "-m", "streamlit", "run", str(home_file)], check=False)
