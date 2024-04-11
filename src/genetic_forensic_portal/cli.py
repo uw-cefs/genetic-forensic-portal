e0a57bcb556edea5ed4ae5ee983f980e4eae2e71
nikiburggraf <nikib@uw.edu>
2024-04-11 15:31:26 -0700

    fix: Add return type annotation to CLI

--- src/genetic_forensic_portal/cli.py
+++ src/genetic_forensic_portal/cli.py
@@ -7,5 +7,5 @@ here = Path(__file__).parent
 home_file = here / "app/Home.py"
 
 
-def up():
+def up() -> None:
     subprocess.run(["python", "-m", "streamlit", "run", str(home_file)], check=False)
