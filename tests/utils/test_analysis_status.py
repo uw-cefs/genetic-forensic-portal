0ab60fb5605af013e80efa08f564fcbdfb39ff1b
nikiburggraf <nikib@uw.edu>
2024-05-21 16:24:38 -0700

    test: add testing for AnalysisStatus ordering

---
+++ tests/utils/test_analysis_status.py
@@ -0,0 +1,19 @@
+from genetic_forensic_portal.utils.analysis_status import AnalysisStatus
+
+
+def test_ordering():
+    """Test the ordering of the AnalysisStatus enum.
+    Order should be
+    ANALYSIS_SUCCEEDED < ANALYSIS_IN_PROGRESS < ANALYSIS_FAILED < ANALYSIS_NOT_FOUND < ANALYSIS_ERROR
+    """
+    # less than
+    assert AnalysisStatus.ANALYSIS_SUCCEEDED < AnalysisStatus.ANALYSIS_IN_PROGRESS
+    assert AnalysisStatus.ANALYSIS_IN_PROGRESS < AnalysisStatus.ANALYSIS_FAILED
+    assert AnalysisStatus.ANALYSIS_FAILED < AnalysisStatus.ANALYSIS_NOT_FOUND
+    assert AnalysisStatus.ANALYSIS_NOT_FOUND < AnalysisStatus.ANALYSIS_ERROR
+
+    # greater than
+    assert AnalysisStatus.ANALYSIS_ERROR > AnalysisStatus.ANALYSIS_NOT_FOUND
+    assert AnalysisStatus.ANALYSIS_NOT_FOUND > AnalysisStatus.ANALYSIS_FAILED
+    assert AnalysisStatus.ANALYSIS_FAILED > AnalysisStatus.ANALYSIS_IN_PROGRESS
+    assert AnalysisStatus.ANALYSIS_IN_PROGRESS > AnalysisStatus.ANALYSIS_SUCCEEDED
