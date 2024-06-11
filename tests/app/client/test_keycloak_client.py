d96f3fda628a5cbe4d538213dc5aa3dce010701e
nikiburggraf <nikib@uw.edu>
2024-06-11 15:22:44 -0700

    Fix tests

--- tests/app/client/test_keycloak_client.py
+++ tests/app/client/test_keycloak_client.py
@@ -23,7 +23,7 @@ def test_login_error_throws():
     with mock.patch(
         "genetic_forensic_portal.app.client.keycloak_client.keycloak_openid"
     ) as mock_keycloak_openid:
-        mock_keycloak_openid.token.side_effect = Exception("Error")
+        mock_keycloak_openid.token.side_effect = KeycloakPostError("Error")
         with pytest.raises(KeycloakPostError):
             keycloak_client.login_user("username", "password")
         assert mock_keycloak_openid.token.call_count == 1
@@ -42,7 +42,7 @@ def test_logout_error_throws():
     with mock.patch(
         "genetic_forensic_portal.app.client.keycloak_client.keycloak_openid"
     ) as mock_keycloak_openid:
-        mock_keycloak_openid.logout.side_effect = Exception("Error")
+        mock_keycloak_openid.logout.side_effect = KeycloakPostError("Error")
         with pytest.raises(KeycloakPostError):
             keycloak_client.logout_user({"refresh_token": "test"})
         assert mock_keycloak_openid.logout.call_count == 1
